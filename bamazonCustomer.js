10196006848f701e650caa523330e21835b6c22b
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-09-03 12:38:59 -0400

    inquirer set up and functional at basic level, now need to put use to product id and amount

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,12 +11,49 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function(err, res) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
 
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    console.log(res);
+  // showProductsTable();
+  takeOrder();
+  
   });
-});
+
+// function showProductsTable(err){
+// 	connection.query("SELECT * FROM products", function(err, res) {
+//     if (err) throw err;
+//     console.log("Products Available:");
+//     console.log(res);
+// });
+// };
+
+function takeOrder (){
+	inquirer.prompt ([
+		{
+      type: "input",
+      message: "What is the ID of the product that you would like to buy?",
+      name: "productID"
+    	},
+    	 {
+      type: "input",
+      message: "How many items of this product would you like to buy?",
+      name: "productAmount"
+    },
+		])
+	 .then(function(answer) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+    // if (inquirerResponse.confirm) {
+    //   console.log("\nWelcome " + inquirerResponse.username);
+    //   console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
+    // }
+    // else {
+    //   console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+    // }
+    console.log(answer);
+  });
+}
+
+
+/* 
+*/
