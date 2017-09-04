47be86ae0a217351eca867dbda714f05eeddeb70
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-09-04 16:38:37 -0400

    yay finally  able to finally use query input to search data in the table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -41,7 +41,7 @@ function inputOrder (){
 	 .then(function(answer) {
 	connection.query("SELECT * FROM products WHERE id=?", [answer.id], function(err, res) {
 		for (var i = 0; i < res.length; i++) {
-      console.log(res[i].id + " | " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name);
+      console.log(res[i].id + " | " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
     }
     })
   });
